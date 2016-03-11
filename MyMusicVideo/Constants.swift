6c0fe41b9e5345f9402d1497fa737e8d650f38b6
Gary Palima <gary.palima@gmail.com>
2016-03-11 23:05:19 +0300

    Part-4 Completed Creating Class Video

---
+++ MyMusicVideo/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  MyMusicVideo
+//
+//  Created by Gary on 3/11/16.
+//  Copyright © 2016 Gary. All rights reserved.
+//
+
+import Foundation
+
+typealias JSONDictionary = [String: AnyObject]
+
+typealias JSONArray = Array<AnyObject>
+
