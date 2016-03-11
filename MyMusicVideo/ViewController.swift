24e6b4522bc0320d3af236544b383328cf2571c6
Gary Palima <gary.palima@gmail.com>
2016-03-11 13:24:44 +0300

    Initial Commit

---
+++ MyMusicVideo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMusicVideo
+//
+//  Created by Gary on 3/11/16.
+//  Copyright © 2016 Gary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
